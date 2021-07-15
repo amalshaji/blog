1a2f9a9a85d1ab0e194ca5bcda62c86db0653f0e
Amal Shaji <18011385+amalshaji@users.noreply.github.com>
2021-07-15 10:15:48 +0530

    init

---
+++ data/authors/default.md
@@ -0,0 +1,14 @@
+---
+name: Amal Shaji
+avatar: /static/images/logo.png
+occupation: Indie
+# company: Stanford University
+email: amalshajid@gmail.com
+twitter: https://twitter.com/pydantic
+# linkedin: https://www.linkedin.com
+github: https://github.com/amalshaji
+---
+
+Amal is a programmer, currently exploring Python, Go, Rust, Svelte and React.
+
+He writes on different platforms including [personal](/projects), [hashnode](https://blog.amalshaji.com), [dev](https://dev.to/amal). He is also a contributor to [Testdrivenio](https://testdriven.io/authors/shaji)
---
+++ data/projectsData.js
@@ -0,0 +1,20 @@
+const projectsData = [
+  {
+    title: 'Phoney',
+    description: `Phoney is a mock API generator based on JSON payload. Features of phoney include are: create apps 
+    to manage fake endpoints, Create multiple endpoints per app, support GET, POST, PUT, DELETE methods, support dynamic endpoints, 
+    Swagger UI for each application, automatic API key protection.`,
+    imgSrc: '/static/images/phoney.jpg',
+    href: 'https://phoney.ml',
+  },
+  {
+    title: 'Amalytics',
+    description: `Amalytics is an experimental analytics platform written in Flask and HarperDB 
+    which won the Hashnode-HarperDB hackathon. Currently, I have made some changes to the project 
+    and deciding whether to make it a self hosted template`,
+    imgSrc: '/static/images/amalytics.jpg',
+    href: 'https://github.com/amalshaji/amalytics',
+  },
+]
+
+export default projectsData
